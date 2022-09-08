5c43201d9e20fb6bf3484ee6d1a19e9ee90c5f45
Nicolas Ramos <95194980+NickHunterD3V@users.noreply.github.com>
2022-09-07 22:29:27 -0300

    now people that aren't beta testers cannot use mine

--- src/commands/game/mine.js
+++ src/commands/game/mine.js
@@ -10,6 +10,7 @@ Loot is different for each pickaxe and can change depending in which world you a
 	usage: 'mine',
 	display: true,
 	code: async d => {
+				if ( d.author.roles.cache.has("967309071604654080")) return channel.send("error:you're not a beta tester!")
 		if (!d.gameDb.get('started', `_${d.author.id}`)) return d.utils.sendError(d, 'notStarted')
 		
 		let inventory = d.gameDb.get("inventory", `_${d.author.id}`)
